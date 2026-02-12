842464fcc53ef7f13497fb578cca09aa66164bcd
Hyarlei Freitas <hyarleysf@gmail.com>
2026-02-12 17:26:21 -0300

    fix: ajustando regras da entidade Appointment

--- backend/src/entities/appointment.ts
+++ backend/src/entities/appointment.ts
@@ -2,8 +2,11 @@ import { randomUUID } from "crypto";
 
 export interface AppointmentProps {
   customer: string;
+  provider: string;
   startAt: Date;
   endAt: Date;
+  createdAt?: Date;
+  canceledAt?: Date;
 }
 
 export class Appointment {
@@ -18,6 +21,10 @@ export class Appointment {
     return this.props.customer;
   }
 
+  get provider() {
+    return this.props.provider;
+  }
+
   get startAt() {
     return this.props.startAt;
   }
@@ -26,10 +33,18 @@ export class Appointment {
     return this.props.endAt;
   }
 
+  get createdAt() {
+    return this.props.createdAt || null;
+  }
+
+  get canceledAt() {
+    return this.props.canceledAt || null;
+  }
+
   constructor(props: AppointmentProps, id?: string) {
     const { startAt, endAt } = props;
+    const duration = endAt.getTime() - startAt.getTime();
 
-    // Validações
     if (startAt <= new Date()) {
       throw new Error('Invalid start date');
     }
@@ -38,8 +53,22 @@ export class Appointment {
       throw new Error('Invalid end date');
     }
 
-    // Atribuições
+    if (!props.customer || props.customer.trim() === '') {
+      throw new Error('Customer is required');
+    }
+
+    if (!props.provider || props.provider.trim() === '') {
+      throw new Error('Provider is required');
+    }
+
+    if (duration < 30 * 60 * 1000) {
+      throw new Error('Appointment must be at least 30 minutes long')
+    }
+
     this._id = id ?? randomUUID();
-    this.props = props;
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
   }
 }
